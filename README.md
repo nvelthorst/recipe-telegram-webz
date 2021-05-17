8263cb15d1f821a809cfcf857f125db6ca1d5b1e
Nick Velthorst <nick.velthorst@label305.com>
2021-05-17 19:49:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Telegram Webz for Franz
+Since web.telegram.org is a piece of shit, we can now use webz.telegram.org
---
+++ webview.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = (Franz) => {
+  const getMessages = function getMessages() {
+    let count = 0;
+    const searchElement = document.querySelector('.SearchInput');
+    if (searchElement) {
+      const elements = document.querySelectorAll('.Badge.unread:not(.muted)');
+      if (elements) {
+
+        for (let i = 0; i < elements.length; i += 1) {
+          if (elements[i].innerHTML !== 0) {
+            count += parseInt(elements[i].innerHTML);
+          }
+        }
+      }
+    }
+    Franz.setBadge(count);
+    return count;
+  };
+
+  Franz.injectCSS(path.join(__dirname, 'service.css'));
+  Franz.loop(getMessages);
+};
